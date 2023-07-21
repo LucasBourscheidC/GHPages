ac708ed7ae2740bd9a40013ff577c6f6411903c9
LucasBourscheidC <lucasbc12022001@gmail.com>
2023-07-21 19:36:06 -0300

    new themes

--- FrontendMentor - Projects/calculator-app-main/initButtons.js
+++ FrontendMentor - Projects/calculator-app-main/initButtons.js
@@ -6,17 +6,31 @@ export function initButtons(newCharacter, reset, result, del)
   const buttonResult = document.querySelector('#result');
   const buttonReset = document.querySelector('#reset');
   const buttonDel = document.querySelector('#del');
+  const buttonFloat = document.querySelector('#float');
   
   buttonReset.addEventListener('click', reset);
   buttonDel.addEventListener('click', del);
   buttonResult.addEventListener('click', result);
+  buttonFloat.addEventListener('click', ()=>{
+    newCharacter(".");
+  })
   themesButton.addEventListener("click", () => {
     if(parseInt(getComputedStyle(themesButton).gridColumn) === 1)
+    {
       themesButton.style.gridColumn = "2";
+      document.body.classList.add('white-theme');
+    }
     else if(parseInt(getComputedStyle(themesButton).gridColumn) === 2)
+    {
       themesButton.style.gridColumn = "3";
+      document.body.classList.remove('white-theme');
+      document.body.classList.add('purple-theme');
+    }
     else
+    {
       themesButton.style.gridColumn = "1";
+      document.body.classList.remove('purple-theme');
+    }
   })
   
   buttonsNumber.forEach(currentButton => {
